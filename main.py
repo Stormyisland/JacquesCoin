d6f5f975dc649b7979fdb944afb58c4346356d87
Stormyisland <jacqueslavoie2015@gmail.com>
2024-05-25 14:11:34 -0700

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-import hashlib
+from Block import Block
 
-hash = hashlib.sha256("secret message" .encode()).hexdigest()
-print(hash)
+
+blockchain = []
+
+genesis_block = Block("My genesis block", ["Satoshi is KING!", 
+                                        "Satoshi is lord",
+                                        "Satoshi is GOD!"])
+
+print(genesis_block.block_hash)
+
+
+
+
+
+
+
+
+
+
+#hash = hashlib.sha256("secret message" .encode()).hexdigest()
+#print(hash)
