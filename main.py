e1fce02e9cd696b8f8a648a4da6ccc4cb1b52586
Stormyisland <jacqueslavoie2015@gmail.com>
2024-05-28 13:07:15 -0700

    2nd commit

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ from Block import Block
 
 blockchain = []
 
-genesis_block = Block("My genesis block", ["Satoshi is KING!", 
-                                        "Satoshi is lord",
-                                        "Satoshi is GOD!"])
+genesis_block = Block("My genesis block", ["Satoshi sent 1 BTC KING!", 
+                                        "Satoshi sent 2 BTC lord",
+                                        "Satoshi sent 3 BTC GOD!"])
 
+second_block = Block(genesis_block.block_hash,["Satoshi sent 3 BTC theodore!"
+                                            "Satoshi sent 3 BTC jacques!"
+                                            "Satoshi sent 3 BTC to bill!"])
+
+third_block =Block(second_block.block_hash, ["Satoshi sent 30 BTC theodore!"
+                                            "Satoshi sent 60  BTC jacques!"
+                                            "Satoshi sent 3 BTC to bill!"])
+
+print("Block hash: Genesis Block")
 print(genesis_block.block_hash)
 
+print("Block hash: Second Block")
+print(second_block.block_hash)
+
+print("Block hash: Third_block")
+print(third_block.block_hash)
 
 
 
