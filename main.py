055a1bf9ef8032e8a480a6f129b53916a09f4ba6
Stormyisland <jacqueslavoie2015@gmail.com>
2024-05-30 23:14:21 -0700

    commiting again

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ second_block = Block(genesis_block.block_hash,["Satoshi sent 3 BTC theodore!"
                                             "Satoshi sent 3 BTC jacques!"
                                             "Satoshi sent 3 BTC to bill!"])
 
-third_block =Block(second_block.block_hash, ["Satoshi sent 30 BTC theodore!"
-                                            "Satoshi sent 60  BTC jacques!"
-                                            "Satoshi sent 3 BTC to billy!"])
+third_block =Block(second_block.block_hash, ["Satoshi sent 300 BTC theodore!"
+                                            "Satoshi sent 600  BTC jacques!"
+                                            "Satoshi sent 30 BTC to billy!"])
 
 print("Block hash: Genesis Block 1")
 print(genesis_block.block_hash)
 
-print("Block hash: Second Block")
+print("Block hash: Second Block 2")
 print(second_block.block_hash)
 
-print("Block hash: Third_block")
+print("Block hash: Third_block 3")
 print(third_block.block_hash)
 
 
